da7c89f19ad9709888aadce9aea204da809de042
Seraphimu <serahuimu@outlook>
2023-10-15 21:42:53 +0800

    test

---
+++ Codes/Java/java_2023_10_15/test2.java
@@ -0,0 +1,12 @@
+public class test2
+{
+    public static void main(String [] args)
+    {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.println("i = " + i);
+                System.out.println("j = " + j);
+            }
+        }
+    }
+}
