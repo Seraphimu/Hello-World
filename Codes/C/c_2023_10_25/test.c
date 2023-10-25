7cbd4de331572aaaa74e3ad4689bf7c873037591
Seraphimu <serahuimu@outlook.com>
2023-10-25 23:37:53 +0800

    c primer plus p149

--- Codes/C/c_2023_10_25/test.c
+++ Codes/C/c_2023_10_25/test.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
+    char ch = '$';
+
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%c", ch);
+        }
+        putchar('\n');
+    }
 
     return 0;
 }
