6d41ffcc48ebf31ba1e40a52c24d9c6ef2a189a7
Seraphimu <serahuimu@outlook.com>
2024-04-26 15:24:05 +0800

    codes

---
+++ Languages/CSharp/CSharpGettingStart/CSharpGettingStart/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+                Console.WriteLine(arr[i]);
+            }
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
