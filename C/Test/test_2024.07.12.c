79c5003029100cf5528b7e98efe1154d1f06c9b0
Seraphimu <serahuimu@outlook.com>
2024-07-13 18:41:51 +0800

    C

---
+++ C/Test/test_2024.07.12.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    unsigned int a = 114514;
+    int b = -1;
+
+    if (a > b) {
+        printf("114514\n");
+    }
+    else {
+        printf("-1\n");
+    }
+
+    return 0;
+}
